d7ca471480fff2c440682b79f56b6cc523cd3278
Travis Nice <travis.nice@everydaypublishing.com.au>
2017-04-26 15:40:30 +1000

    Added widget functionality to the front page
    Tested and working on localhost

--- front-page.php
+++ front-page.php
@@ -2,6 +2,6 @@
 if ( 'posts' == get_option( 'show_on_front' ) ) {
     include( get_home_template() );
 } else {
-    get_template_part( 'template-parts/content', 'front' );
+    get_template_part( 'template-parts/widgeted', 'front' );
 }
 ?>
